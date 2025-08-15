8e2d1b0631bd7dd552d8ea610a69671c898157ba
qzeeo <naitechinc@gmail.com>
2025-08-15 02:57:00 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TELEGRAM_API_KEY = '7553953860:AAHOzNjZWvL74vLAOOUveTpWqfoRAKROsjc' # Your telegram bot api Key
+TELEGRAM_API_KEY = '' # Your telegram bot api Key
 COINBASE_COMMERCE_API_KEY = '' # Put your coinbase commerce api key
 YOUR_ADMIN_ID = 0000000  # Replace with your admin Telegram ID
